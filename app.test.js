a393dffb87580807e80a3324744f8986d30180e7
Anchangwan <ssdii66@gmail.com>
2025-11-26 07:23:19 +0000

    test PASS

--- app.test.js
+++ app.test.js
@@ -2,38 +2,21 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Express App Tests', () => {
-  // afterAll((done) => {
-  //   server.close(done);
+
+  // describe('GET /', () => {
+  //   test('should return Hello World', async () => {
+  //     const res = await request(app).get('/');
+  //     expect(res.statusCode).toBe(200);
+  //     expect(res.text).toBe('Hello World');
+  //   });
   // });
-
-  // test('GET / should return Hello World message', async () => {
-  //   const response = await request(app).get('/');
-  //   expect(response.statusCode).toBe(200);
-  //   expect(response.body).toEqual({ message: 'Hello World!' });
-  // });
- const request = require('supertest');
-const app = require('./app');
-
-describe('Express App Tests', () => {
-
-  const request = require('supertest');
-const app = require('./app');
-
-describe('Express App Tests', () => {
-
-  describe('GET /', () => {
-    test('should return Hello World', async () => {
-      const res = await request(app).get('/');
-      expect(res.statusCode).toBe(200);
-      expect(res.text).toBe('Hello World');
-    });
-  });
 
   describe('GET /health', () => {
     test('should return healthy status', async () => {
       const response = await request(app).get('/health');
       expect(response.statusCode).toBe(200);
-     expect(response.body).toEqual({ status: 'healthy' });
-     });
+      expect(response.body).toEqual({ status: 'healthy' });
+    });
   });
+
 });
