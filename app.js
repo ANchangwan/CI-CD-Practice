80046e8471542c8bacd84e1075d2223cb49caadb
waney <ssdii66@gmail.com>
2025-11-28 18:59:11 +0900

    테스트 argoCD

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'argo CD!!!!' });
+  res.json({ message: '테스트 argoCD' });
 });
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'healthy!!' });
 });
 
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module) {
+  // 직접 실행될 때만 서버 시작
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-module.exports = { app, server };
+module.exports = app;
+
--- app.test.js
+++ app.test.js
@@ -1,15 +1,11 @@
 const request = require('supertest');
-const { app, server } = require('./app');
+const app = require('./app'); // 서버를 시작하지 않고 app만 import
 
 describe('Express App Tests', () => {
-  afterAll((done) => {
-    server.close(done);
-  });
-
   test('GET / should return Hello World message', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ message: 'argo CD!!!!' });
+    expect(response.body).toEqual({ message: '테스트 argoCD' });
   });
 
   test('GET /health should return healthy status', async () => {
